1eb66afcb841cc9f4e9eeab149ea298d11df7548
00sahilaleem00 <00sahilaleem00@gmail.com>
2020-09-29 17:00:22 +0530

    WoowWoow

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
 var ball;
+var database;
+var reference, referenceValue, referencePosition;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database = firebase.database();
+    reference = database.ref("body/position");
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+
+    referenceValue = reference.on("value",addPosition);
+
+    if(referencePosition !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            changePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            changePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            changePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            changePosition(0,+1);
+        }
     }
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    reference.set({
+        "x": referencePosition.x + x,
+        "y": referencePosition.y +y
+    })
+}
+
+function addPosition(data) {
+    referencePosition = data.val();
+    ball.x = referencePosition.x;
+    ball.y = referencePosition.y;
 }
